34c08efb0e4a3b39037e3cd3d8215c422857e224
Jared J <26422790+ipaqmaster@users.noreply.github.com>
2025-12-10 11:45:54 +1100

    Initial stub commit of python rewrite.
    It's about time.

---
+++ modules/VFIO.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#from dateutil import parser
+#import datetime
+#import hashlib
+#import json
+#import os
+#import re
+#import time
+
+#Usage:
+#   from modules.VFIO import VFIO
+#   vfio = VFIO(config=configDict)
+#   vfio.run()
+
+class VFIO:
+    def __init__(self, args):
+
+        self.args = args
+
+    def run(self):
+        print('Stub commit.')
+        print('Soon this will do something.')
+        return(True)
+
+
+
+
+
+
